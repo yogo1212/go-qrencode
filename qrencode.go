ec29e3a58f148d69f2bf7fb3b61f6e3d6b85dda6
Leon M. George <leon@georgemail.eu>
2016-11-14 23:08:17 +0100

    init

---
+++ qrencode.go
@@ -0,0 +1,113 @@
+package qrencode
+
+/*
+#cgo CFLAGS: -I/usr/include/
+#cgo LDFLAGS: -L/usr/lib/ -lqrencode
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <qrencode.h>
+
+const char *strerrno(void) {
+	return strerror(errno);
+}
+*/
+import "C"
+
+import (
+	"errors"
+)
+
+type QRCode struct {
+	Version int
+	Lines [][]bool
+}
+
+type QRECLevel int
+
+const (
+	QR_ECLEVEL_L QRECLevel = iota
+	QR_ECLEVEL_M           = iota
+	QR_ECLEVEL_Q           = iota
+	QR_ECLEVEL_H           = iota
+)
+
+var qrECLevel = map[QRECLevel]C.QRecLevel {
+	QR_ECLEVEL_L: C.QR_ECLEVEL_L,
+	QR_ECLEVEL_M: C.QR_ECLEVEL_M,
+	QR_ECLEVEL_Q: C.QR_ECLEVEL_Q,
+	QR_ECLEVEL_H: C.QR_ECLEVEL_H,
+}
+
+type QRMode int
+
+const (
+  QR_MODE_NUM QRMode = iota
+  QR_MODE_AN         = iota
+  QR_MODE_8          = iota
+  QR_MODE_KANJI      = iota
+  QR_MODE_STRUCTURE  = iota
+  QR_MODE_ECI        = iota
+  QR_MODE_FNC1FIRST  = iota
+  QR_MODE_FNC1SECOND = iota
+)
+
+var qrMode = map[QRMode]C.QRencodeMode {
+	QR_MODE_NUM: C.QR_MODE_NUM,
+	QR_MODE_AN: C.QR_MODE_AN,
+	QR_MODE_8: C.QR_MODE_8,
+	QR_MODE_KANJI: C.QR_MODE_KANJI,
+	QR_MODE_STRUCTURE: C.QR_MODE_STRUCTURE,
+	QR_MODE_ECI: C.QR_MODE_ECI,
+	QR_MODE_FNC1FIRST: C.QR_MODE_FNC1FIRST,
+	QR_MODE_FNC1SECOND: C.QR_MODE_FNC1SECOND,
+}
+
+var qrCaseSensitive = map[bool]C.int {
+	false: 0,
+	true: 1,
+}
+
+func qrmeh(q *C.QRcode) (qr QRCode) {
+	qr.Version = int(q.version)
+	qr.Lines = make([][]bool, q.width)
+
+	b := C.GoBytes(q.data, q.width*q.width)
+
+	for i, _ := range qr.Lines {
+		for _, x := range b[0:q.width] {
+			qr.Lines[i] = append(qr.Lines[i], (x & 1) == 1)
+		}
+		b = b[q.width:]
+	}
+
+	return
+}
+
+func QRCodeEncodeString(what string, version int, level QRECLevel, hint QRMode, caseSensitive bool) (qr QRCode, err error) {
+	s := C.CString(what)
+	defer C.free(s)
+	q := C.QRcode_encodeString(s, C.int(version), qrECLevel[level], qrMode[hint], qrCaseSensitive[caseSensitive])
+	if q == nil {
+		err = errors.New(C.GoString(C.strerrno()))
+		return
+	}
+	defer C.QRcode_free(q)
+
+	qr = qrmeh(q)
+	return
+}
+
+func QRCodeEncodeStringMQR(what string, version int, level QRECLevel, hint QRMode, caseSensitive bool) (qr QRCode, err error) {
+	s := C.CString(what)
+	defer C.free(s)
+	q := C.QRcode_encodeStringMQR(s, C.int(version), qrECLevel[level], qrMode[hint], qrCaseSensitive[caseSensitive])
+	if q == nil {
+		err = errors.New(C.GoString(C.strerrno()))
+		return
+	}
+	defer C.QRcode_free(q)
+
+	qr = qrmeh(q)
+	return
+}
